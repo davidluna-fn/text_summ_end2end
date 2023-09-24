c6b6ce3c5d352b230d77eb225f4815e47c31f992
David <davidluna.fn@gmail.com>
2023-09-24 18:21:09 -0500

    feat: create data ingestion pipeline

---
+++ main.py
@@ -0,0 +1,13 @@
+from SummaVerse.pipeline.step_01_data_ingestion import DataIngestionTrainingPipeline
+from SummaVerse.logging import logger
+
+STAGE_NAME = 'Data Ingestion stage'
+
+try:
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<< ')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.run()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<< \n\nx=====================x')
+except Exception as e:
+    logger.exception(e)
+    raise e
