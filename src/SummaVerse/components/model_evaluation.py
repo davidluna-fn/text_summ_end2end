639c71ec79c251a77f04a78e3fbf78b3fba11c25
David <davidluna.fn@gmail.com>
2023-10-01 13:35:02 -0500

    bug(model_prediction): update parameter in score metric

--- src/SummaVerse/components/model_evaluation.py
+++ src/SummaVerse/components/model_evaluation.py
@@ -59,7 +59,7 @@ class ModelEvaluation:
         rouge_names = ['rouge1', 'rouge2', 'rougeL', 'rougeLsum']
         rouge_metric = load_metric('rouge')
 
-        score = self.calculate_metric_on_test_ds(dataset_samsum_pt['test'][0:10], rouge_metric, model_pegasus, tokenizer, batch_size=2, column_text='article', column_summary='highlights', device=device)
+        score = self.calculate_metric_on_test_ds(dataset_samsum_pt['test'][0:10], rouge_metric, model_pegasus, tokenizer, batch_size=2, column_text='dialogue', column_summary='summary', device=device)
 
         rouge_dict = dict((rn, score[rn].mid.fmeasure) for rn in rouge_names)
 
